127c6c30cf8a00070cfa3b175715918f2e7cf267
Filipe Fernandes <filipefirmino50@gmail.com>
2022-03-21 17:31:48 -0300

    Exercicio matriz21

--- src/listas_exercicss/ExerMatriz.java
+++ src/listas_exercicss/ExerMatriz.java
@@ -13,27 +13,39 @@ public class ExerMatriz {
 		int m,n;
 		System.out.println("Informe um valor inteiro: ");
 		m = sc.nextInt();
-		sc.nextLine();
+		//sc.nextLine();
 		System.out.println("Informe outro valor inteiro: ");
 		n = sc.nextInt();
-		sc.nextLine();
-		
-		
-		
-		
+		//sc.nextLine();
+						
 		int[][] mat = new int[m][n];
 		
 		System.out.println("Informe um valor para X: ");
 		int x = sc.nextInt();
-		sc.nextLine();
+		//sc.nextLine();
 		
 		for(int i = 0;i < m; i++) {
-			for(int j = 0;i < n; i++) {
+			for(int j = 0;j < n; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i = 0;i < m; i++) {
+			for(int j = 0;j < n; j++) {
 				if(mat[i][j] == x) {
-					System.out.println("A esquerda: " + mat[i][j-1] );
-					System.out.println("Acima " + mat[i-1][j] );
-					System.out.println("A direita: " + mat[i][j+1] );
+					if(j > 0) {
+						System.out.println("A esquerda: " + mat[i][j-1] );
+					}
+					if(i > 0) {
+						System.out.println("Acima " + mat[i-1][j] );
+					}
+					if(j < mat[i].length-1) {
+						System.out.println("A direita: " + mat[i][j+1] );
+					}
+					if(i < mat.length-1) {
 					System.out.println("Abaixo: " + mat[i+1][j] );
+					}
+					
 				}
 				
 			}
