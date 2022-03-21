aeb31d4e32131bb3c4ba2a9de51bbb84b681e952
Filipe Fernandes <filipefirmino50@gmail.com>
2022-03-21 17:09:11 -0300

    Inicio exercicio

---
+++ src/listas_exercicss/ExerMatriz.java
@@ -0,0 +1,45 @@
+package listas_exercicss;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExerMatriz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int m,n;
+		System.out.println("Informe um valor inteiro: ");
+		m = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Informe outro valor inteiro: ");
+		n = sc.nextInt();
+		sc.nextLine();
+		
+		
+		
+		
+		int[][] mat = new int[m][n];
+		
+		System.out.println("Informe um valor para X: ");
+		int x = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i = 0;i < m; i++) {
+			for(int j = 0;i < n; i++) {
+				if(mat[i][j] == x) {
+					System.out.println("A esquerda: " + mat[i][j-1] );
+					System.out.println("Acima " + mat[i-1][j] );
+					System.out.println("A direita: " + mat[i][j+1] );
+					System.out.println("Abaixo: " + mat[i+1][j] );
+				}
+				
+			}
+		}
+		
+		sc.close();
+	}
+
+}
